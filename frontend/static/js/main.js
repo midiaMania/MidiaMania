d22fec293c3db7f0ac1cca484134d45135fb6f2d
mateusalmeiida <mateusar222@gmail.com>
2023-12-02 10:15:00 -0300

    FEAT: improved profile menu using hidden

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -3,9 +3,46 @@
 
 const menu = document.getElementById("menu");
 const menuBtn = document.getElementById("menuBtn");
+const menuProfile_1 = document.getElementById("menuProfile_1");
+const menuProfile_2 = document.getElementById("menuProfile_2");
+const menuProfile_3 = document.getElementById("menuProfile_3");
+const menuProfile_4 = document.getElementById("menuProfile_4");
+const menuProfileBtn_1 = document.getElementById("menuProfileBtn_1");
+const menuProfileBtn_2 = document.getElementById("menuProfileBtn_2");
+const menuProfileBtn_3 = document.getElementById("menuProfileBtn_3");
+const menuProfileBtn_4 = document.getElementById("menuProfileBtn_4");
+
 
 menuBtn.addEventListener("click", () => {
     menu.classList.toggle("hidden-menu");
+});
+
+menuProfileBtn_1.addEventListener("click", () => {
+    menuProfile_1.classList.remove("hidden");
+    menuProfile_2.classList.add("hidden");
+    menuProfile_3.classList.add("hidden");
+    menuProfile_4.classList.add("hidden");
+});
+
+menuProfileBtn_2.addEventListener("click", () => {
+    menuProfile_1.classList.add("hidden");
+    menuProfile_2.classList.remove("hidden");
+    menuProfile_3.classList.add("hidden");
+    menuProfile_4.classList.add("hidden");
+});
+
+menuProfileBtn_3.addEventListener("click", () => {
+    menuProfile_1.classList.add("hidden");
+    menuProfile_2.classList.add("hidden");
+    menuProfile_3.classList.remove("hidden");
+    menuProfile_4.classList.add("hidden");
+});
+
+menuProfileBtn_4.addEventListener("click", () => {
+    menuProfile_1.classList.add("hidden");
+    menuProfile_2.classList.add("hidden");
+    menuProfile_3.classList.add("hidden");
+    menuProfile_4.classList.remove("hidden");
 });
 
 /* Scroll */

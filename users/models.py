2ed2a2cf9b4002276d23f2a6e893db9bc4ff344b
pauloh-fm <phfarmar@gmail.com>
2023-12-05 02:51:17 -0300

    [feat] add userProfileModel in users_models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+    cpf = models.CharField(max_length=14)  # Adicionei o campo CPF
+    city = models.CharField(max_length=255)
+    neighborhood = models.CharField(max_length=255)
+    number = models.CharField(max_length=10)
+    street = models.CharField(max_length=255)
+    cep = models.CharField(max_length=10)
+    complement = models.CharField(max_length=255)
+

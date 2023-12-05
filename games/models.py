e32e082295733d1aaac99e652d1abfc51f003c83
pauloh-fm <phfarmar@gmail.com>
2023-12-05 12:14:42 -0300

    [feat] add games models

--- games/models.py
+++ games/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    platform = models.CharField(max_length=255)
+    developer = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=255)
+    release_date = models.DateField()
+    rating = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='frontend/static/images/games/')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title

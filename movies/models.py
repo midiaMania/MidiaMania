50cadd8f9c61bf50ba684ca2aa926539e6c0339e
pauloh-fm <phfarmar@gmail.com>
2023-12-05 02:32:23 -0300

    [feat] add movies models

--- movies/models.py
+++ movies/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
-# class Category(models.Model):
-#     name = models.CharField(max_length=255)
+class Category(models.Model):
+    name = models.CharField(max_length=255)
 
-# class Movie(models.Model):
-#     title = models.CharField(max_length=255)
-#     synopsis = models.TextField()
-#     categories = models.ManyToManyField(Category)
-#     directors = models.CharField(max_length=255)
-#     release_date = models.DateField()
-#     running_time = models.IntegerField()  # Em minutos
-#     rating = models.IntegerField()
-#     price = models.DecimalField(max_digits=5, decimal_places=2)
-#     img = models.CharField(max_length=255)
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    synopsis = models.TextField()
+    categories = models.ManyToManyField(Category)
+    directors = models.CharField(max_length=255)
+    release_date = models.DateField()
+    running_time = models.IntegerField()  # Em minutos
+    rating = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    img = models.CharField(max_length=255)
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.title
 
 #TODO: VERIFCAR SE HAVERA UM NUMERO FICTICIO DE COPIAS, SE SERÁ REALMENTE UM LINK PARA IMAGEM
 

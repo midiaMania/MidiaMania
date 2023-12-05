65544e9bc16ef6601ebd4eaeac2ed922c52907be
pauloh-fm <phfarmar@gmail.com>
2023-12-05 02:31:01 -0300

    [feat] add models music

--- musics/models.py
+++ musics/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class MusicCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+class Music(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    record_label = models.CharField(max_length=255)
+    release_date = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    num_tracks = models.IntegerField()
+    format = models.CharField(max_length=255)
+    img = models.CharField(max_length=255)
+    categories = models.ManyToManyField(MusicCategory)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title

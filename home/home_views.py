0658824ea4835c5816e0462e46822c00a6e9898d
mateusalmeiida <mateusar222@gmail.com>
2023-12-09 15:44:07 -0300

    FEAT: template home now works dynamically

--- home/home_views.py
+++ home/home_views.py
@@ -1,205 +1,45 @@
 from django.shortcuts import render
+from movies.models import Movie
+from games.models import Game
+from musics.models import Music
 
 
 def home(request):
+    movies = Movie.objects.all()[:10]
+    musics = Music.objects.all()[:10]
+    games = Game.objects.all()[:10]
+
+    movies_list=[]
+    musics_list=[]
+    games_list=[]
+    
+    for movie in movies:
+            movies_list.append({
+                'name': movie.title,
+                'price': "{:,.2f}".format(movie.price).replace('.', ','),
+                'img': movie.img.url,
+                'slug': movie.slug
+        })
+
+    for game in games:
+            games_list.append({
+                'name': game.title,
+                'price': "{:,.2f}".format(game.price).replace('.', ','),
+                'img': game.image.url,
+                'slug': game.slug
+        })
+            
+    for music in musics:
+            musics_list.append({
+                'name': music.name,
+                'price': "{:,.2f}".format(music.price).replace('.', ','),
+                'img': music.image.url,
+                'slug': music.slug
+        })
+            
     context = {
-        'movies': [
-            {
-                'name': "Titanic",
-                'price': "{:,.2f}".format(10).replace('.', ','),
-                'img': "movies/images/1.jpg",
-                'slug': "titanic"
-            },
-            {
-                'name': "MIB",
-                'price': "{:,.2f}".format(14).replace('.', ','),
-                'img': "movies/images/2.jpg",
-                'slug': "mib"
-            },
-            {
-                'name': "Interstellar",
-                'price': "{:,.2f}".format(10).replace('.', ','),
-                'img': "movies/images/3.png",
-                'slug': "interstellar"
-            },
-            {
-                'name': "Avengers: Endgame",
-                'price': "{:,.2f}".format(20).replace('.', ','),
-                'img': "movies/images/4.jpg",
-                'slug': "avengers-endgame"
-            },
-            {
-                'name': "Liga da Justiça",
-                'price': "{:,.2f}".format(22).replace('.', ','),
-                'img': "movies/images/5.jpg",
-                'slug': "liga-da-justica"
-            },
-            {
-                'name': "Harry Potter",
-                'price': "{:,.2f}".format(14).replace('.', ','),
-                'img': "movies/images/6.jpg",
-                'slug': "harry-potter"
-            },
-            {
-                'name': "Titanic",
-                'price': "{:,.2f}".format(10).replace('.', ','),
-                'img': "movies/images/1.jpg",
-                'slug': "titanic"
-            },
-            {
-                'name': "MIB",
-                'price': "{:,.2f}".format(14).replace('.', ','),
-                'img': "movies/images/2.jpg",
-                'slug': "mib"
-            },
-            {
-                'name': "Interstellar",
-                'price': "{:,.2f}".format(10).replace('.', ','),
-                'img': "movies/images/3.png",
-                'slug': "interstellar"
-            },
-            {
-                'name': "Avengers: Endgame",
-                'price': "{:,.2f}".format(20).replace('.', ','),
-                'img': "movies/images/4.jpg",
-                'slug': "avengers-endgame"
-            }
-        ],
-        'games': [
-            {
-                'name': "Zelda",
-                'price': "{:,.2f}".format(140).replace('.', ','),
-                'img': "games/images/1.jpeg",
-                'slug': "zelda"
-            },
-            {
-                'name': "Doom",
-                'price': "{:,.2f}".format(20).replace('.', ','),
-                'img': "games/images/2.jpg",
-                'slug': "doom"
-            },
-            {
-                'name': "Watch Dogs",
-                'price': "{:,.2f}".format(40).replace('.', ','),
-                'img': "games/images/3.jpg",
-                'slug': "watch-dogs"
-            },
-            {
-                'name': "Spider-man",
-                'price': "{:,.2f}".format(120).replace('.', ','),
-                'img': "games/images/4.jpg",
-                'slug': "spider-man"
-            },
-            {
-                'name': "Assassin's Creed",
-                'price': "{:,.2f}".format(50).replace('.', ','),
-                'img': "games/images/5.jpg",
-                'slug': "assassins-creed"
-            },
-            {
-                'name': "Red Alert 3",
-                'price': "{:,.2f}".format(20).replace('.', ','),
-                'img': "games/images/6.jpg",
-                'slug': "red-alert-3"
-            },
-            {
-                'name': "Zelda",
-                'price': "{:,.2f}".format(140).replace('.', ','),
-                'img': "games/images/1.jpeg",
-                'slug': "zelda"
-            },
-            {
-                'name': "Doom",
-                'price': "{:,.2f}".format(20).replace('.', ','),
-                'img': "games/images/2.jpg",
-                'slug': "doom"
-            },
-            {
-                'name': "Watch Dogs",
-                'price': "{:,.2f}".format(40).replace('.', ','),
-                'img': "games/images/3.jpg",
-                'slug': "watch-dogs"
-            },
-            {
-                'name': "Spider-man",
-                'price': "{:,.2f}".format(120).replace('.', ','),
-                'img': "games/images/4.jpg",
-                'slug': "spider-man"
-            },
-            {
-                'name': "Assassin's Creed",
-                'price': "{:,.2f}".format(50).replace('.', ','),
-                'img': "games/images/5.jpg",
-                'slug': "assassins-creed"
-            },
-            {
-                'name': "Red Alert 3",
-                'price': "{:,.2f}".format(20).replace('.', ','),
-                'img': "games/images/6.jpg",
-                'slug': "red-alert-3"
-            }
-        ],
-        'musics': [
-            {
-                'name': "Kid Abelha",
-                'price': "{:,.2f}".format(4).replace('.', ','),
-                'img': "musics/images/1.jpg",
-                'slug': "kid-abelha"
-            },
-            {
-                'name': "The Beatles",
-                'price': "{:,.2f}".format(5).replace('.', ','),
-                'img': "musics/images/2.jpg",
-                'slug': "the-beatles"
-            },
-            {
-                'name': "Iron Maiden",
-                'price': "{:,.2f}".format(7).replace('.', ','),
-                'img': "musics/images/3.jpg",
-                'slug': "iron-maiden"
-            },
-            {
-                'name': "Linkin Park",
-                'price': "{:,.2f}".format(10).replace('.', ','),
-                'img': "musics/images/4.jpg",
-                'slug': "linkin-park"
-            },
-            {
-                'name': "Michael Jackson",
-                'price': "{:,.2f}".format(14).replace('.', ','),
-                'img': "musics/images/5.jpg",
-                'slug': "michael-jackson"
-            },
-            {
-                'name': "Kid Abelha",
-                'price': "{:,.2f}".format(4).replace('.', ','),
-                'img': "musics/images/1.jpg",
-                'slug': "kid-abelha"
-            },
-            {
-                'name': "The Beatles",
-                'price': "{:,.2f}".format(5).replace('.', ','),
-                'img': "musics/images/2.jpg",
-                'slug': "the-beatles"
-            },
-            {
-                'name': "Iron Maiden",
-                'price': "{:,.2f}".format(7).replace('.', ','),
-                'img': "musics/images/3.jpg",
-                'slug': "iron-maiden"
-            },
-            {
-                'name': "Linkin Park",
-                'price': "{:,.2f}".format(10).replace('.', ','),
-                'img': "musics/images/4.jpg",
-                'slug': "linkin-park"
-            },
-            {
-                'name': "Michael Jackson",
-                'price': "{:,.2f}".format(14).replace('.', ','),
-                'img': "musics/images/5.jpg",
-                'slug': "michael-jackson"
-            }
-        ]
+        'movies': movies_list,
+        'musics': musics_list,
+        'games': games_list
     }
     return render(request, "home/home.html", context)
